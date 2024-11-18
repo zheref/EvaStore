2c8caed7dbd4e44bab9ec22f9f77cba9e32a495d
Sergio Daniel <me@zheref.io>
2024-11-18 08:55:59 -0500

    Add simple list of mocked books

--- Swift/EvaStore/EvaStore/Application/ViewController.swift
+++ Swift/EvaStore/EvaStore/Application/ViewController.swift
@@ -9,6 +9,35 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    let books: [Book] = [
+        .init(title: "The Alchemist",
+              coverPicture: nil,
+              author: .init(name: "Paulo Coelho",
+                            nationality: "Brazilian",
+                            birthDate: nil,
+                            genres: [.fiction, .fantasy]),
+              publicationDate: Date(),
+              genre: .fiction),
+        
+        Book(title: "The Great Gatsby",
+             coverPicture: nil,
+             author: .init(name: "F. Scott Fitzgerald",
+                           nationality: "American",
+                           birthDate: nil,
+                           genres: [.fiction, .fantasy]),
+             publicationDate: Date(),
+             genre: .fiction),
+        
+        .init(title: "The Da Vinci Code",
+              coverPicture: nil,
+              author: .init(name: "Dan Brown",
+                            nationality: "American",
+                            birthDate: nil,
+                            genres: [.fiction, .thriller]),
+              publicationDate: Date(),
+              genre: .fiction),
+    ]
+    
     @IBOutlet weak var booksTableView: NSTableView!
     
 
