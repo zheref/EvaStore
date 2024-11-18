de862ee6fcd7d2b7dcaa849e912531f9e49413ac
Sergio Daniel <me@zheref.io>
2024-11-18 07:54:56 -0500

    Initial commit

---
+++ Swift/EvaStore/EvaStore/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EvaStore
+//
+//  Created by Sergio Daniel on 11/18/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
