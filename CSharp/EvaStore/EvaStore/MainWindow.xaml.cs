d37ee16e4607f03caf328534608058b4c894cca5
Sergio Daniel <me@zheref.io>
2024-11-19 11:49:50 -0500

    Create entites and mocked data for C#

--- CSharp/EvaStore/EvaStore/MainWindow.xaml.cs
+++ CSharp/EvaStore/EvaStore/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using ABI.Windows.ApplicationModel.Store.Preview.InstallControl;
+using EvaStore.Model;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,6 +25,27 @@ namespace EvaStore
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private Book[] books = [
+            new(
+                title: "The Alchemist", 
+                author: new("Paulo Coelho", "Brazilian", [Genre.Fiction, Genre.Fantasy]),
+                publicationTime:DateTime.Now,
+                genre: Genre.Fiction
+            ),
+            new(
+                title: "The Great Gatsby",
+                author: new("F. Scott Fitzgerald", "American", [Genre.Fiction, Genre.Fantasy]),
+                publicationTime:DateTime.Now,
+                genre: Genre.Fiction
+            ),
+            new(
+                title: "The Alchemist",
+                author: new("Dan Brown", "American", [Genre.Fiction, Genre.Thriller]),
+                publicationTime:DateTime.Now,
+                genre: Genre.Fiction
+            )
+        ];
+
         public MainWindow()
         {
             this.InitializeComponent();
---
+++ CSharp/EvaStore/EvaStore/Model/Entities.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+using System;
+
+namespace EvaStore.Model;
+
+enum Genre
+{
+    Fiction, Romance, SelfHelp,
+    History, Fantasy, Thriller
+}
+
+struct Author
+{
+    internal string name;
+    internal string nationality;
+    internal DateTime? birthDate;
+    internal Genre[] genres = [];
+
+    internal Author(string name, string nationality, Genre[] genres)
+    {
+        this.name = name;
+        this.nationality = nationality;
+        this.genres = genres;
+    }
+}
+
+struct Book
+{
+    internal string title;
+    internal Uri? coverPicture;
+    internal Author author;
+    internal DateTime publicationTime;
+    internal Genre genre;
+
+    internal Book(string title, Author author, DateTime publicationTime,
+        Genre genre)
+    {
+        this.title = title;
+        this.author = author;
+        this.publicationTime = publicationTime;
+        this.genre = genre;
+    }
+}
+#nullable disable
