24ea57eb1fa658829261908a2840601c48728331
Sergio Daniel <me@zheref.io>
2024-11-20 09:15:17 -0500

    First unit test base

---
+++ Swift/EvaStore/EvaStoreTests/Application/ViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerTests.swift
+//  EvaStoreTests
+//
+//  Created by Sergio Daniel on 11/20/24.
+//
+
+import XCTest
+
+final class ViewControllerTests: XCTestCase {
+
+    
+
+}
